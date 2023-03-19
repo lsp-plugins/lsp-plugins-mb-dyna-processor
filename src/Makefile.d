fa4d901458ca6214b20c3ad771fd0f03e87f15f7
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-19 15:21:40 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -410,6 +410,7 @@ $(LSP_PLUGINS_MB_DYNA_PROCESSOR_BIN)/main/ui/mb_dyna_processor.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPlayListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ProxyPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
