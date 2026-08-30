b87fd25dd45b8286226dac8c83647600b260d511
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-08-30 19:25:16 +0300

    Optimized CPU consumption: FFT analysis is not performed when UI is not active

--- src/main/plug/mb_dyna_processor.cpp
+++ src/main/plug/mb_dyna_processor.cpp
@@ -769,18 +769,24 @@ namespace lsp
 
         void mb_dyna_processor::ui_activated()
         {
-            size_t channels     = (nMode == MBDP_MONO) ? 1 : 2;
+            const size_t channels   = (nMode == MBDP_MONO) ? 1 : 2;
 
             for (size_t i=0; i<channels; ++i)
             {
-                channel_t *c        = &vChannels[i];
+                channel_t * const c     = &vChannels[i];
 
                 for (size_t j=0; j<c->nPlanSize; ++j)
                 {
-                    dyna_band_t *b      = c->vPlan[j];
-                    b->nSync            = S_ALL;
+                    dyna_band_t * const b   = c->vPlan[j];
+                    b->nSync                = S_ALL;
                 }
             }
+            sAnalyzer.set_activity(true);
+        }
+
+        void mb_dyna_processor::ui_deactivated()
+        {
+            sAnalyzer.set_activity(false);
         }
 
         uint32_t mb_dyna_processor::decode_sidechain_type(uint32_t sc) const
@@ -826,7 +832,6 @@ namespace lsp
 
             // Determine number of channels
             size_t channels     = (nMode == MBDP_MONO) ? 1 : 2;
-            int active_channels = 0;
             size_t env_boost    = pEnvBoost->value();
 
             // Determine work mode: classic, modern or linear phase
@@ -885,11 +890,6 @@ namespace lsp
                 sAnalyzer.enable_channel(c->nAnInChannel, c->bInFft);
                 sAnalyzer.enable_channel(c->nAnOutChannel, c->pFftOutSw->value()  >= 0.5f);
 
-                if (sAnalyzer.channel_active(c->nAnInChannel))
-                    active_channels ++;
-                if (sAnalyzer.channel_active(c->nAnOutChannel))
-                    active_channels ++;
-
                 // Update envelope boost filters
                 if ((env_boost != nEnvBoost) || (bEnvUpdate))
                 {
@@ -933,7 +933,6 @@ namespace lsp
             sAnalyzer.set_reactivity(pReactivity->value());
             if (pShiftGain != NULL)
                 sAnalyzer.set_shift(pShiftGain->value() * 100.0f);
-            sAnalyzer.set_activity(active_channels > 0);
 
             // Update analyzer
             if (sAnalyzer.needs_reconfiguration())
